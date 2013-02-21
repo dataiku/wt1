5a905cbfd315e60ae5aa7834ad7e1187cdce944a
Clement Stenac <clement.stenac@dataiku.com>
2013-02-21 09:28:16 +0100

    Script to create a source tarball

---
+++ make-source-tarball.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+VERSION=$1
+
+if [ -z $VERSION ] 
+then 
+  echo "$0 version"
+  exit 1
+fi
+
+DIRNAME=wt1-src-$VERSION
+DIR=dist/src-tarball/$DIRNAME
+
+rm -rf $DIR
+mkdir -p $DIR
+git clone https://github.com/dataiku/wt1.git $DIR
+cd $DIR
+rm -rf .git
+cd ..
+tar cz $DIRNAME > ../wt1-$VERSION.tar.gz
